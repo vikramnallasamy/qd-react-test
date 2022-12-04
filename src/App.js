47cc27776479c3fbd705c4e2ab180bb6ad57f745
vikramnallasamy <vikramn1991@gmail.com>
2022-12-04 14:30:52 +0530

    first view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import styled from 'styled-components';
 import './App.scss';
+import TestIcons from './components/TestIcons';
 
 function App() {
   return (
-    <div className='App'>
-        Welcome
-    </div>
+        <AppContainer>
+          <HeaderSection>Header</HeaderSection>
+          <SideBarSection></SideBarSection>
+          <ContentSection>
+            <TestIcons />
+          </ContentSection>
+        </AppContainer>
       );
 }
+
+const HeaderSection = styled.section `
+  grid-area: header;
+  `
+const SideBarSection = styled.section `
+  grid-area: sidebar;
+  max-width: 200px;
+  `
+const ContentSection = styled.section `
+  grid-area: content;
+  `
+const AppContainer = styled.div `
+  display: grid;
+  height: 100vh;
+  grid-template-columns: minmax(40px,100px) minmax(400px, auto);
+  grid-template-rows: minmax(40px,80px) 9fr;
+  grid-template-areas: 'header header' 'sidebar content'; 
+  & > * {
+    outline: 1px solid black;
+    min-height: 10px;
+  }
+  `
 
 export default App;
---
+++ src/components/TestIcons.js
@@ -0,0 +1,32 @@
+import CouponDiscount from '../icons/Coupon Discount.svg';
+import customer from '../icons/customer.svg';
+import filter from '../icons/filter.svg';
+import NotificationBell from '../icons/Notification Bell.svg';
+import orders from '../icons/orders.svg';
+import Package from '../icons/Package.svg';
+import pen from '../icons/pen.svg';
+import plus from '../icons/plus.svg';
+import SEARCH from '../icons/SEARCH.svg';
+import seettings from '../icons/seettings.svg';
+import threedots from '../icons/threedots.svg';
+import user from '../icons/user.svg';
+
+    export default function TestIcons() {
+    return (
+        <div>
+            <img title='Coupon Discount' src={CouponDiscount} />
+			<img title='customer' src={customer} />
+			<img title='filter' src={filter} />
+			<img title='Notification Bell' src={NotificationBell} />
+			<img title='orders' src={orders} />
+			<img title='Package' src={Package} />
+			<img title='pen' src={pen} />
+			<img title='plus' src={plus} />
+			<img title='SEARCH' src={SEARCH} />
+			<img title='seettings' src={seettings} />
+			<img title='threedots' src={threedots} />
+			<img title='user' src={user} />
+			
+        </div>)
+    }
+
